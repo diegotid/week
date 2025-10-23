66bee1fcb232b7005c4ae7285495dc89398a5615
Diego Rivera <diego@cuatro.studio>
2025-10-24 00:17:11 +0200

    Clean up menus

---
+++ Week/About.swift
@@ -0,0 +1,62 @@
+//
+//  About.swift
+//  Week
+//
+//  Created by Diego Rivera on 24/10/25.
+//
+
+import SwiftUI
+
+struct About: View {
+    private var appIconImage: Image? = {
+        guard let image = NSImage(named: "AppIcon") else {
+            return nil
+        }
+        return Image(nsImage: image)
+    }()
+
+    private var appVersionLabel: Text? = {
+        guard let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String else {
+            return nil
+        }
+        return Text("Version \(version)")
+    }()
+    
+    var body: some View {
+        VStack {
+            if appIconImage != nil {
+                appIconImage!
+                    .resizable()
+                    .cornerRadius(18)
+                    .frame(maxWidth: Frame.aboutIconSize, maxHeight: Frame.aboutIconSize)
+            }
+            Text("Week Number")
+                .font(.title)
+                .bold()
+                .padding(.bottom, 10)
+            if appVersionLabel != nil {
+                appVersionLabel
+                    .font(.subheadline)
+                    .padding(.bottom, 10)
+            }
+            Text("© 2025 Diego Rivera")
+            if let website = URL(string: "http://cuatro.studio") {
+                Link("cuatro.studio", destination: website)
+            }
+        }
+        .padding()
+        .frame(width: Frame.aboutWindowWidth, height: Frame.aboutWindowHeight)
+    }
+}
+
+enum Frame {
+    static let aboutWindowWidth: CGFloat = 240
+    static let aboutWindowHeight: CGFloat = 260
+    static let aboutIconSize: CGFloat = 100
+    static let menuBarHeight: Int = 20
+}
+
+#Preview {
+    About()
+}
+
--- Week/WeekApp.swift
+++ Week/WeekApp.swift
@@ -9,10 +9,41 @@ import SwiftUI
 
 @main
 struct WeekApp: App {
+    @Environment(\.openWindow) var openWindow
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .fixedWindow(size: CGSize(width: 820, height: 660))
         }
+        Window("About Week Number", id: "about") {
+            About()
+        }
+        .commands {
+            CommandGroup(replacing: .appInfo) {
+                Button("About Week Number") {
+                    openWindow(id: "about")
+                }
+            }
+            CommandGroup(replacing: .newItem) { }
+            CommandGroup(replacing: .saveItem) { }
+            CommandGroup(replacing: .importExport) { }
+            CommandGroup(replacing: .printItem) { }
+            CommandGroup(replacing: .toolbar) { }
+            CommandGroup(replacing: .sidebar) { }
+            CommandGroup(replacing: .windowArrangement) { }
+            CommandGroup(replacing: .undoRedo) { }
+            CommandGroup(replacing: .pasteboard) { }
+        }
+        .commands {
+            CommandGroup(replacing: .textEditing) { }
+            CommandGroup(replacing: .help) { }
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        NSWindow.allowsAutomaticWindowTabbing = false
     }
 }
